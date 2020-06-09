7e62ae82fc5da62a5d77c2090e60db85c868f4ff
Theo Sun <theo.sun@sap.com>
2020-06-09 15:05:50 +0800

    feat: service discovery

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
+import express from "express";
+import process from "process";
+import { MicroService } from "./sd";
 
-export const a = 1;
+const app = express();
+const port = parseInt(process.env.PORT || '3000', 10);
 
+app.use(express.json());
 
+app.get("/api/v1/catalog", req => {
+    req.res.json({
+        status: 200,
+        data: []
+    });
+});
+
+app.listen(port, () => {
+    const ms = new MicroService({ application: "micro-commerce", service: "service-catalog", version: "v0.0.1", port });
+    process.on("SIGINT", async () => {
+        await ms.stop();
+        process.exit(0);
+    });
+});
